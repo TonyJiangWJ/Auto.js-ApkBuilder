ed5e1f0f83ade71d1858b86d5948bc237c92717a
TonyJiangWJ <tonyjiangwj@gmail.com>
2023-07-19 09:13:43 +0800

    首次修复打包插件功能

--- apkbuilder/src/main/java/com/stardust/autojs/apkbuilder/ApkPackager.java
+++ apkbuilder/src/main/java/com/stardust/autojs/apkbuilder/ApkPackager.java
@@ -1,5 +1,7 @@
 package com.stardust.autojs.apkbuilder;
 
+import android.text.TextUtils;
+
 import com.stardust.autojs.apkbuilder.util.StreamUtils;
 
 import java.io.File;
@@ -36,7 +38,7 @@ public class ApkPackager {
         ZipInputStream zis = new ZipInputStream(mApkInputStream);
         for (ZipEntry e = zis.getNextEntry(); e != null; e = zis.getNextEntry()) {
             String name = e.getName();
-            if (!e.isDirectory()) {
+            if (!e.isDirectory() && !TextUtils.isEmpty(name)) {
                 File file = new File(mWorkspacePath, name);
                 System.out.println(file);
                 file.getParentFile().mkdirs();
--- apkbuilder/src/test/java/com/stardust/autojs/apkbuilder/ExampleUnitTest.java
+++ apkbuilder/src/test/java/com/stardust/autojs/apkbuilder/ExampleUnitTest.java
@@ -1,8 +1,8 @@
 package com.stardust.autojs.apkbuilder;
 
-import com.cmy.parser.ArscReader;
-import com.cmy.parser.ResTablePrinter;
-import com.cmy.parser.bean.ResTable;
+//import com.cmy.parser.ArscReader;
+//import com.cmy.parser.ResTablePrinter;
+//import com.cmy.parser.bean.ResTable;
 
 import org.junit.Test;
 
@@ -25,21 +25,21 @@ import pxb.android.axml.Util;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
-        File manifest = new File("AndroidManifest.xml");
+//        File manifest = new File("AndroidManifest.xml");
 
     }
 
     @Test
     public void test() throws Exception {
-        AxmlReader reader = new AxmlReader(Util.readFile(new File("AndroidManifest1.xml")));
-        reader.accept(new DumpAdapter());
+//        AxmlReader reader = new AxmlReader(Util.readFile(new File("AndroidManifest1.xml")));
+//        reader.accept(new DumpAdapter());
     }
 
     @Test
     public void test1() throws IOException {
-        ArscReader reader = new ArscReader(new File("resources.arsc"));
-        PrintStream printStream = new PrintStream(new File("out.txt"), "utf-8");
-        ResTable resTable = reader.read();
-        new ResTablePrinter(resTable, printStream).printResTablePackage(true);
+//        ArscReader reader = new ArscReader(new File("resources.arsc"));
+//        PrintStream printStream = new PrintStream(new File("out.txt"), "utf-8");
+//        ResTable resTable = reader.read();
+//        new ResTablePrinter(resTable, printStream).printResTablePackage(true);
     }
 }
